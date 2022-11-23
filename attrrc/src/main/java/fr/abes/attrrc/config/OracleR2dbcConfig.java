ba9664ec303eca6eabc034c5220c2c5ce7bd8082
charot <charot@abes.fr>
2022-11-23 18:24:05 +0100

    Test 3 connexions et maxIdle 1 minute

--- attrrc/src/main/java/fr/abes/attrrc/config/OracleR2dbcConfig.java
+++ attrrc/src/main/java/fr/abes/attrrc/config/OracleR2dbcConfig.java
@@ -58,7 +58,7 @@ public class OracleR2dbcConfig {
                         .connectionRetryInterval(30, TimeUnit.SECONDS)
                         // the maximum number of connections in the pool
                         //.maxPoolSize(Runtime.getRuntime().availableProcessors() * 5)
-                        .maxPoolSize(Runtime.getRuntime().availableProcessors())
+                        .maxPoolSize(3)
                         //.maxPoolSize(3)
                         .build();
 
--- linked_rc_idref_sudoc/src/main/java/fr/abes/linked_rc_idref_sudoc/config/OracleR2dbcConfig.java
+++ linked_rc_idref_sudoc/src/main/java/fr/abes/linked_rc_idref_sudoc/config/OracleR2dbcConfig.java
@@ -52,7 +52,7 @@ public class OracleR2dbcConfig {
                         .connectionRetryInterval(30, TimeUnit.SECONDS)
                         // the maximum number of connections in the pool
                         //.maxPoolSize(Runtime.getRuntime().availableProcessors() * 5)
-                        .maxPoolSize(Runtime.getRuntime().availableProcessors())
+                        .maxPoolSize(3)
                         //.maxPoolSize(3)
                         .build();
 
